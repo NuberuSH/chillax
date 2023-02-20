26e794fb36df88596e774490bfae231bc22bd004
Daniel Tallón <daniel.tallon.ortega@gmail.com>
2023-02-20 19:52:58 +0100

    change images name to be readable by humans

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ async function runLandingPage () {
   const main = document.getElementById('landingpage-main');
   const curtain = document.getElementById('curtain');
   const images = [
-    '04hRKjIlrjUWVlGJkApTAhz-27.fit_lim.size_1600x900.v1623769787.jpg',
-    '62879d2e0537fc6a778fe9d3_Hint_Home_Header.jpg',
-    'Teenage-Girl-Gamer-Wearing-Headset-Gaming-1140x570.jpg',
-    'The-5-Key-Ingredients-To-A-Good-Movie.webp',
-    'depositphotos_freeimage.jpg',
-    'getty_841135958_351803.jpg',
-    'group-of-men-and-women-playing-video-games.jpg',
-    'iStock-1001414360.jpg',
-    'shutterstock_1301620948-62f17d21eac12-sej-1520x800.webp',
-    'woman-watching-scary-movie-at-night.jpg',
-    'young-man-playing-video-game-royalty-free-image-1572450323.jpg'
+    'backgroundImage1.jpg',
+    'backgroundImage2.jpg',
+    'backgroundImage3.jpg',
+    'backgroundImage4.webp',
+    'backgroundImage5.jpg',
+    'gbackgroundImage6.jpg',
+    'backgroundImage7.jpg',
+    'backgroundImage8.jpg',
+    'backgroundImage9.webp',
+    'backgroundImage10.jpg',
+    'backgroundImage11.jpg'
   ];
 
   let position = Math.floor(Math.random() * images.length);
-  
+
   while (true) {
     main.style.backgroundImage = `url("${images[position]}")`;
     curtain.style.opacity = 0;
