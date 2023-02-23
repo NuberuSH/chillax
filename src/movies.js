73c73003582c67ed67df594383e712229dabab97
Adrián Jiménez <adrianjimenez1994@gmail.com>
2023-02-23 16:54:14 +0100

    bug en el filtro arreglado

--- src/movies.js
+++ src/movies.js
@@ -25,40 +25,7 @@ let genre = 0;
 console.log(page);
 // let ul = document.getElementById("movie-list");
 let list = document.getElementById("movie-list");
-function getMovieList() {
-  //Realizamos la petición a la api
-  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&page=${page}`)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
-    for (let i = 0; i < data.results.length; i++) {
-      // let li = document.createElement("li");
-      // li.innerText = data.results[i].title;
-      let divMovie = document.createElement("div");
-      divMovie.className = "movie";
-      let img = document.createElement("img");
-      img.className = "poster";
-      img.src = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
-      img.alt = data.results[i].title;
-      let divMovieFooter = document.createElement("div");
-      divMovieFooter.className = "movie-footer";
-      let footerTitle = document.createElement("div");
-      footerTitle.className = "title";
-      footerTitle.innerText = data.results[i].title;
-      divMovieFooter.appendChild(footerTitle);
-      divMovie.appendChild(img);
-      divMovie.appendChild(divMovieFooter);
 
-      list.appendChild(divMovie);
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
-}
-
-window.onload = getMovieList;
 
 //PARA PAGINAR LISTA DE PELÍCULAS
 // let prev = document.getElementById("previous");
@@ -71,8 +38,8 @@ window.onload = getMovieList;
 // if (page === 500) {
 //   next.visibility = "hidden";
 // }
-//Petición a la api para añadir los options al select que filtrará por género
 
+//Petición a la api para añadir los options al select que filtrará por género
 fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES`)
   .then(response => {
     return response.json();
@@ -94,17 +61,19 @@ fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=039e4f7f61c4c831908
 function showMore() {
   page++;
   if (genre !== 0) {
-    genreFilterApi();
+    getMovieList();
   } else {
-    let content = "";
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&page=${page}`)
+    showMoreDefault();
+  }
+}
+
+function showMoreDefault() {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=${+page}`)
     .then(response => {
       return response.json();
     })
     .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
       for (let i = 0; i < data.results.length; i++) {
-        // let li = document.createElement("li");
-        // li.innerText = data.results[i].title;
         let divMovie = document.createElement("div");
         divMovie.className = "movie";
         let img = document.createElement("img");
@@ -126,44 +95,42 @@ function showMore() {
     .catch(error => {
       console.error(error);
     });
-  }
-  
 }
 
 //Esta función disminuye en 1 el valor de page, realizando una petición a la página anterior de la api
-function previousPage() {
-  page--;
-  let content = "";
-  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&page=${page}`)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
-    for (let i = 0; i < data.results.length; i++) {
-      // let li = document.createElement("li");
-      // li.innerText = data.results[i].title;
-      let divMovie = document.createElement("div");
-      divMovie.className = "movie";
-      let img = document.createElement("img");
-      img.className = "poster";
-      img.src = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
-      img.alt = data.results[i].title;
-      let divMovieFooter = document.createElement("div");
-      divMovieFooter.className = "movie-footer";
-      let footerTitle = document.createElement("div");
-      footerTitle.className = "title";
-      footerTitle.innerText = data.results[i].title;
-      divMovieFooter.appendChild(footerTitle);
-      divMovie.appendChild(img);
-      divMovie.appendChild(divMovieFooter);
+// function previousPage() {
+//   page--;
+//   let content = "";
+//   fetch(`https://api.themoviedb.org/3/movie/popular?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&page=${page}`)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
+//     for (let i = 0; i < data.results.length; i++) {
+//       // let li = document.createElement("li");
+//       // li.innerText = data.results[i].title;
+//       let divMovie = document.createElement("div");
+//       divMovie.className = "movie";
+//       let img = document.createElement("img");
+//       img.className = "poster";
+//       img.src = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
+//       img.alt = data.results[i].title;
+//       let divMovieFooter = document.createElement("div");
+//       divMovieFooter.className = "movie-footer";
+//       let footerTitle = document.createElement("div");
+//       footerTitle.className = "title";
+//       footerTitle.innerText = data.results[i].title;
+//       divMovieFooter.appendChild(footerTitle);
+//       divMovie.appendChild(img);
+//       divMovie.appendChild(divMovieFooter);
 
-      list.appendChild(divMovie);
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
-}
+//       list.appendChild(divMovie);
+//     }
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+// }
 // function getMovies(id) {
 //   fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES`)
 //   .then(response => {
@@ -193,43 +160,73 @@ function previousPage() {
 function genreFilter() {
   page = 1;
   genre = document.getElementById("genre-filter").value;
-  console.log(`El género seleccionado es ${genre} y la página ${page}`);
-  if (genre === "0") {
-    console.log("Estoy entrando al default");
-    getMovieList();
-  }
   list.innerHTML = "";
-  genreFilterApi();
+  getMovieList();
 }
 
-function genreFilterApi() {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=${+page}&with_genres=${+genre}`)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
-    for (let i = 0; i < data.results.length; i++) {
-      // let li = document.createElement("li");
-      // li.innerText = data.results[i].title;
-      let divMovie = document.createElement("div");
-      divMovie.className = "movie";
-      let img = document.createElement("img");
-      img.className = "poster";
-      img.src = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
-      img.alt = data.results[i].title;
-      let divMovieFooter = document.createElement("div");
-      divMovieFooter.className = "movie-footer";
-      let footerTitle = document.createElement("div");
-      footerTitle.className = "title";
-      footerTitle.innerText = data.results[i].title;
-      divMovieFooter.appendChild(footerTitle);
-      divMovie.appendChild(img);
-      divMovie.appendChild(divMovieFooter);
+function getMovieList() {
+  if (genre == 0) {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=${+page}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
+      for (let i = 0; i < data.results.length; i++) {
+        // let li = document.createElement("li");
+        // li.innerText = data.results[i].title;
+        let divMovie = document.createElement("div");
+        divMovie.className = "movie";
+        let img = document.createElement("img");
+        img.className = "poster";
+        img.src = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
+        img.alt = data.results[i].title;
+        let divMovieFooter = document.createElement("div");
+        divMovieFooter.className = "movie-footer";
+        let footerTitle = document.createElement("div");
+        footerTitle.className = "title";
+        footerTitle.innerText = data.results[i].title;
+        divMovieFooter.appendChild(footerTitle);
+        divMovie.appendChild(img);
+        divMovie.appendChild(divMovieFooter);
 
-      list.appendChild(divMovie);
+        list.appendChild(divMovie);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+    } else {
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=039e4f7f61c4c831908c02f8c3e9aba0&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=${+page}&with_genres=${+genre}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => { //Recibimos la respuesta de la api (data) y recorremos la lista que contiene (results) para mostrar las películas en el html
+      for (let i = 0; i < data.results.length; i++) {
+        // let li = document.createElement("li");
+        // li.innerText = data.results[i].title;
+        let divMovie = document.createElement("div");
+        divMovie.className = "movie";
+        let img = document.createElement("img");
+        img.className = "poster";
+        img.src = `https://image.tmdb.org/t/p/w500${data.results[i].poster_path}`;
+        img.alt = data.results[i].title;
+        let divMovieFooter = document.createElement("div");
+        divMovieFooter.className = "movie-footer";
+        let footerTitle = document.createElement("div");
+        footerTitle.className = "title";
+        footerTitle.innerText = data.results[i].title;
+        divMovieFooter.appendChild(footerTitle);
+        divMovie.appendChild(img);
+        divMovie.appendChild(divMovieFooter);
+
+        list.appendChild(divMovie);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
     }
-  })
-  .catch(error => {
-    console.error(error);
-  });
 }
+
+
+window.onload = getMovieList;
