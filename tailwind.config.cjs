8b0b1f0a6ab19f2e50a2cf1dbb84fb6716258b10
Dani Tallón <kalimero99@protonmail.ch>
2023-03-16 16:25:05 +0100

    reconfigure tailwind and fix styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,6 @@ module.exports = {
           fondo: "#C9D5E0",
           topBar: "#4FA9A9",
           loginButton: "#A5E1AD",
-          formBgDarkMode: "#1b1b32",
           custom1: "#21094E",
           custom2: "#511281",
           custom3: "#4CA1A3",
@@ -19,10 +18,7 @@ module.exports = {
       },
 
       fontFamily: {
-        bluetea: "bluetea",
-        quicksand: "Quicksand",
-        caveat: "Caveat",
-        urbanist: "Urbanist"
+        RobotoSlab: "RobotoSlab"
       }
     },
   },
