2baba35ec86407270b355914df2145ac2343987a
Dani Tallón <kalimero99@protonmail.ch>
2023-03-16 18:58:02 +0100

    reconfigure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,12 @@ module.exports = {
     extend: {
       colors: {
         web: {
-          boton: "#79b5f3",
-          boton2: "#3f67bc",
+          boton: "#8b3030",
+          boton2: "#510d0d",
           fondo: "#C9D5E0",
           topBar: "#4FA9A9",
           loginButton: "#A5E1AD",
+          formBgDarkMode: "#1b1b32",
           custom1: "#21094E",
           custom2: "#511281",
           custom3: "#4CA1A3",
@@ -18,7 +19,7 @@ module.exports = {
       },
 
       fontFamily: {
-        RobotoSlab: "RobotoSlab"
+        robotoslab: "RobotoSlab",
       }
     },
   },
